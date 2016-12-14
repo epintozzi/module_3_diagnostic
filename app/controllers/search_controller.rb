f5a6a167f6577df809f73e787a915bda32befc92
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-14 10:50:07 -0700

    Begin refactoring out to service Add vcr cassettes to gitignore. Oops.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,11 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location='80216'&fuel_type=ELEC,LPG&limit=10")
-# binding.pry
-    parsed_response = JSON.parse(response.body)
+    # response = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params[:q]}&fuel_type=ELEC,LPG&limit=10")
+    #
+    # parsed_response = JSON.parse(response.body)
+    parsed_response = AltFuelFinderService.get_response
     @fuel_stations = parsed_response["fuel_stations"]
-    # @name = parsed_response["fuel_stations"].first["station_name"]
-    # @address
-    # @distance
-    # @access_time
-    # @fuel_type
   end
 
 end
---
+++ app/services/alt_fuel_finder.rb
@@ -0,0 +1,17 @@
+class AltFuelFinderService
+  attr_reader :conn
+
+  def initialize
+    @conn = Faraday.new(:url => 'http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+  def get_response
+    response = conn.get("?api_key=#{ENV['nrel_api_key']}&location=#{params[:q]}&fuel_type=ELEC,LPG&limit=10")
+
+    parsed_response = JSON.parse(response.body)
+  end
+end
