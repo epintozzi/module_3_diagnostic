92d48d210c423f1cc82cc830e1a191f1f5e32c53
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-14 10:31:06 -0700

    Get response to appear in view WIP: Need to pass in zip (currently hard coded) WIP: Need to refactor hash out of view WIP: Need to create Service and model to remove hash from view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,15 @@
 class SearchController < ApplicationController
 
   def index
-    results = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location='80216'&fuel_type=ELEC,LPG&limit=10")
-binding.pry
-    parsed_result = JSON.parse(results.body)
+    response = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location='80216'&fuel_type=ELEC,LPG&limit=10")
+# binding.pry
+    parsed_response = JSON.parse(response.body)
+    @fuel_stations = parsed_response["fuel_stations"]
+    # @name = parsed_response["fuel_stations"].first["station_name"]
+    # @address
+    # @distance
+    # @access_time
+    # @fuel_type
   end
 
 end
