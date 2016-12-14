e8e55f3375b95f413b0cf3f3a3a7a4a06c2abcfb
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-14 10:18:15 -0700

    Work on faraday request in search controller Begin parsing response in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    binding.pry
-    search = Faraday.get("developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=ENV['nrel_api_key']&location='80216'&fuel_type=ELEC,LPG&limit=10")
+    results = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location='80216'&fuel_type=ELEC,LPG&limit=10")
+binding.pry
+    parsed_result = JSON.parse(results.body)
   end
 
 end
